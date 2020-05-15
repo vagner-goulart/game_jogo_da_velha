fca1e24f6ea565abafc846e19ad9a58521f9eb36
vagnerPG <vagnergoulart28@gmail.com>
2020-05-15 15:17:51 -0300

    adicionada termocolor
    Termcolor adicionado. Agora xis ou circ que ganhar são coloridos de uma cor.

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,3 +1,4 @@
+from termcolor import colored,cprint
 import random
 
 linha_1 = [" + "] * 3
@@ -11,6 +12,7 @@ circ = " o "
 jogar_primeiro = random.randint(0, 1)
 
 ninguem_ganhou = False
+
 
 def coloca_xis_ou_circ(xis_ou_circ):
     numero_aleatorio_xis = random.randint(0, 2)
@@ -33,6 +35,8 @@ def coloca_xis_ou_circ(xis_ou_circ):
 
 
 def imprime_linhas():
+    global linha_1, linha_2, linha_3
+
     print("".join(linha_1))
     print("".join(linha_2))
     print("".join(linha_3))
@@ -40,7 +44,7 @@ def imprime_linhas():
 
 
 def logica_perdeu_ganhou():
-    global ninguem_ganhou
+    global ninguem_ganhou, ganhou_em_horizontal, ganhou_em_vertical, ganhou_em_diagonal
 
     diagonal_esq_e_dir = linha_1[0] == linha_2[1] == linha_3[2] or linha_1[2] == linha_2[1] == linha_3[0]
 
@@ -51,13 +55,17 @@ def logica_perdeu_ganhou():
 
     while contador < 3:
 
-        #print("teste", contador)
+        # print("teste", contador)
 
         ganhou_em_horizontal = linhas[contador][0] != " + " and len(set(linhas[contador])) == 1
 
         ganhou_em_vertical = linha_1[contador] != " + " and linha_1[contador] == linha_2[contador] == linha_3[contador]
 
         ninguem_ganhou = ganhou_em_horizontal == ganhou_em_vertical == ganhou_em_diagonal
+
+        if ganhou_em_horizontal or ganhou_em_vertical or ganhou_em_diagonal:
+            colorir_caracteres_venceu(contador)
+            imprime_linhas()
 
         if ganhou_em_vertical:
             print("Ganhou vertical", ganhou_em_vertical)
@@ -77,8 +85,31 @@ def logica_perdeu_ganhou():
         contador += 1
     return venceu
 
+
+def colorir_caracteres_venceu(local_caractere):
+
+    if ganhou_em_horizontal:
+        for i in range(3):
+            linhas[local_caractere][i] = colored(linhas[local_caractere][i], 'green')
+
+    if ganhou_em_vertical:
+        for i in range(3):
+            linhas[i][local_caractere] = colored(linhas[i][local_caractere], 'green')
+
+    if ganhou_em_diagonal:
+        if linha_1[0] == linha_2[1] == linha_3[2]:
+            for i in range(3):
+                linhas[i][i] = colored(linhas[i][i], 'blue')
+
+        if linha_1[2] == linha_2[1] == linha_3[0]:
+            contador_posicao = 0
+            for i in range(2, -1, -1):
+                linhas[i][contador_posicao] = colored(linhas[i][contador_posicao], 'red')
+                contador_posicao += 1
+
+
 def zerar_var_linha_e_linhas():
-    global linha_1, linha_2, linha_3,linhas, jogar_primeiro
+    global linha_1, linha_2, linha_3, linhas, jogar_primeiro
 
     linha_1 = [" + "] * 3
     linha_2 = [" + "] * 3
@@ -90,7 +121,6 @@ def zerar_var_linha_e_linhas():
 
 
 def jogar_testes():
-
     num_jogada = 0
 
     if jogar_primeiro == 1:
@@ -104,30 +134,32 @@ def jogar_testes():
 
         coloca_xis_ou_circ(parametro_xis_ou_circ_0)  # add X
         num_jogada += 1
-        #print("colocou x")
+        # print("colocou x")
         if num_jogada >= 5 and logica_perdeu_ganhou():
             print("X ganhou")
             break
         elif num_jogada >= 9 and ninguem_ganhou:
+            imprime_linhas()
             print("Draw")
             break
 
         coloca_xis_ou_circ(parametro_xis_ou_circ_1)  # add O
         num_jogada += 1
-        #print("colocou O")
+        # print("colocou O")
         if num_jogada >= 5 and logica_perdeu_ganhou():
             print("O ganhou")
             break
         elif num_jogada >= 9 and ninguem_ganhou:
+            imprime_linhas()
             print("Draw")
             break
 
-
-    imprime_linhas()
+    print("¨"*20)
 
     zerar_var_linha_e_linhas()
 
     print()
 
-for x in range(10):
+
+for x in range(25):
     jogar_testes()
